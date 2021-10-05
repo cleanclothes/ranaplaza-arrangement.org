5c0c63c596a2c4a7f921cdfc077ed2ff2b3fd332
Paul Roeland <paul@cleanclothes.org>
2021-10-05 12:37:54 +0200

    duh

---
+++ src/pages/404.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Template from '../components/layout'
+
+const NotFoundPage = () => (
+  <Template>
+    <div id="main" className="alt">
+      <section id="one">
+        <div className="inner">
+          <h1>NOT FOUND</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </div>
+      </section>
+    </div>
+  </Template>
+)
+
+export default NotFoundPage
